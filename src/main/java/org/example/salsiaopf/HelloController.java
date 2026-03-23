dc4b838d7a808ef65bb6cd94e413ab764199cd6a
YhaelGit <yhael.hilariord@gmail.com>
2026-03-23 09:00:14 -0400

    Refactor HelloController to implement Initializable

--- src/main/java/org/example/salsiaopf/HelloController.java
+++ src/main/java/org/example/salsiaopf/HelloController.java
@@ -1,14 +1,55 @@
 package org.example.salsiaopf;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class HelloController implements Initializable {
+
+    @FXML private ImageView logoImage;
+
+    @FXML private ToggleButton segOrder;
+    @FXML private ToggleButton segSupplier;
+    @FXML private ToggleButton segReception;
+    @FXML private ToggleButton segIngredient;
+    @FXML private ToggleButton segPayment;
+
+    @FXML private VBox viewOrder;
+    @FXML private VBox viewSupplier;
+    @FXML private VBox viewReception;
+    @FXML private VBox viewIngredient;
+    @FXML private VBox viewPayment;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+        URL logoUrl = getClass().getResource("/imagenes/logo-salsiao.jpeg");
+        if (logoUrl != null && logoImage != null) {
+            logoImage.setImage(new Image(logoUrl.toExternalForm()));
+        }
+
+        if (segOrder != null) segOrder.setSelected(true);
+        showOnly(viewOrder);
     }
+
+    private void showOnly(VBox target) {
+        VBox[] all = { viewOrder, viewSupplier, viewReception, viewIngredient, viewPayment };
+        for (VBox v : all) {
+            boolean on = (v == target);
+            v.setVisible(on);
+            v.setManaged(on);
+        }
+    }
+
+    @FXML private void showOrder() { showOnly(viewOrder); }
+    @FXML private void showSupplier() { showOnly(viewSupplier); }
+    @FXML private void showReception() { showOnly(viewReception); }
+    @FXML private void showIngredient() { showOnly(viewIngredient); }
+    @FXML private void showPayment() { showOnly(viewPayment); }
 }
